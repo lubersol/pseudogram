5d169b5ebb35a26418e70322a4e1593a8d148b40
Lucia <lubersol@gmail.com>
2021-04-30 19:31:36 +0200

    añadiendo boton login

--- src/App.js
+++ src/App.js
@@ -1,15 +1,26 @@
 import React, { Component } from "react";
-import logo from "./logo.svg";
+import firebase from "firebase";
+
 import "./App.css";
 
 class App extends Component {
+  handleAuth() {
+    const provider = new firebase.auth.GoogleAuthProvider();
+
+    firebase.auth().signInWithPopup(provider)
+      .then(result => console.log(`${result.user.email} Ha iniciado sesión`))
+      .catch(error => console.log(`Error ${error.code}: ${error.message}`));
+  }
+
   render() {
     return (
       <div className="App">
         <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>Pseudogram</p>
+          <h2>Pseudogram</h2>
         </header>
+        <p className="App-intro">
+          <button onClick={this.handleAuth}>Login con Google</button>
+        </p>
       </div>
     );
   }
--- src/index.js
+++ src/index.js
@@ -1,14 +1,25 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom";
+import firebase from "firebase";
+
+import "./index.css";
+import App from "./App";
+import reportWebVitals from "./reportWebVitals";
+
+firebase.initializeApp({
+  apiKey: "AIzaSyBK0d7blp81pIWIVnv0P6yetiJKHZiVAAE",
+  authDomain: "pseudogram-584b5.firebaseapp.com",
+  projectId: "pseudogram-584b5",
+  storageBucket: "pseudogram-584b5.appspot.com",
+  messagingSenderId: "777607774696",
+  appId: "1:777607774696:web:fe881a993c59df0434e45d",
+});
 
 ReactDOM.render(
   <React.StrictMode>
     <App />
   </React.StrictMode>,
-  document.getElementById('root')
+  document.getElementById("root")
 );
 
 // If you want to start measuring performance in your app, pass a function
