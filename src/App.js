17b8754859740b1ebde7605e20d2da91714f2b86
Lucia <lubersol@gmail.com>
2021-05-01 19:15:46 +0200

    añadiendo archivo fileUpload para cargar archivos

--- src/App.js
+++ src/App.js
@@ -1,26 +1,74 @@
 import React, { Component } from "react";
 import firebase from "firebase";
 
+import FileUpload from './FileUpload';
 import "./App.css";
 
 class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      user: null,
+    };
+    this.handleAuth = this.handleAuth.bind(this);
+    this.handleLogout = this.handleLogout.bind(this);
+  }
+
+  //Añadir el objeto usuario al estado, cuando el usuario se loguee.
+  //Metodo ciclo vida
+  componentWillMount() {
+    firebase.auth().onAuthStateChanged((user) => {
+      //modifica el estado
+      this.setState({
+        user,
+      });
+    });
+  }
+  //Métoo del boton para loguearnos
   handleAuth() {
     const provider = new firebase.auth.GoogleAuthProvider();
 
-    firebase.auth().signInWithPopup(provider)
-      .then(result => console.log(`${result.user.email} Ha iniciado sesión`))
-      .catch(error => console.log(`Error ${error.code}: ${error.message}`));
+    firebase
+      .auth()
+      .signInWithPopup(provider)
+      .then((result) => console.log(`${result.user.email} Ha iniciado sesión`))
+      .catch((error) => console.log(`Error ${error.code}: ${error.message}`));
+  }
+
+  handleLogout(){
+    firebase.auth().signOut()
+    .then((result) => console.log(`${result.user.email} Ha salido de la sesión`))
+      .catch((error) => console.log(`Error ${error.code}: ${error.message}`));
+  }
+  renderLoginButton() {
+    //Si el usuario está logueado
+    if (this.state.user) {
+      return (
+        <div>
+          <img
+            src={this.state.user.photoURL}
+            alt={this.state.user.displayName}
+          />
+          <p>Hola {this.state.user.displayName}!</p>
+          <button onClick={this.handleLogout}>Logout</button>
+          <FileUpload />
+        </div>
+      );
+    } else {
+      return(
+      //No está logueado
+      <button onClick={this.handleAuth}>Login con Google</button>
+      );
+    }
   }
 
   render() {
     return (
       <div className="App">
-        <header className="App-header">
+        <div className="App-header">
           <h2>Pseudogram</h2>
-        </header>
-        <p className="App-intro">
-          <button onClick={this.handleAuth}>Login con Google</button>
-        </p>
+        </div>
+        <p className="App-intro">{this.renderLoginButton()}</p>
       </div>
     );
   }
---
+++ src/FileUpload.js
@@ -0,0 +1,25 @@
+import React, { Component } from "react";
+//import firebase from "firebase";
+
+class FileUpload extends Component {
+  constructor() {
+    super();
+    this.state = {
+      uploadValue: 0,
+      picture: null
+    };
+  }
+
+  render() {
+    return (
+      <div>
+        <progress value={this.state.uploadValue} max="100" />
+        <br />
+        <input type="file" onChange={this.props.onUpload} />
+        <br />
+        <img width="320" src={this.state.picture} alt="" />
+      </div>
+    );
+  }
+}
+export default FileUpload;
