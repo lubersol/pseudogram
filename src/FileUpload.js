307b75ff91bea33a628e4cf6adae6253ab59739f
Lucia <lubersol@gmail.com>
2021-05-01 20:10:44 +0200

    cargar imagenes y guardar en firebase

--- src/FileUpload.js
+++ src/FileUpload.js
@@ -1,21 +1,49 @@
 import React, { Component } from "react";
-//import firebase from "firebase";
+import firebase from "firebase";
 
 class FileUpload extends Component {
   constructor() {
     super();
     this.state = {
       uploadValue: 0,
-      picture: null
+      picture: null,
     };
+    this.handleUpload = this.handleUpload.bind(this);
+  }
+
+  handleUpload(event) {
+    const file = event.target.files[0];
+    const storageRef = firebase.storage().ref(`/Fotos/${file.name}`);
+    const task = storageRef.put(file);
+
+    task.on(
+      "state_changed",
+      (snapshot) => {
+        let percentage =
+          (snapshot.bytesTransferred / snapshot.totalBytes) * 100;
+        this.setState({
+          uploadValue: percentage,
+        });
+      },
+      (error) => {
+        console.log(error.message);
+      },
+      () => {
+        storageRef.getDownloadURL().then((url) => {
+          this.setState({ picture: url });
+        });
+      }
+    );
   }
 
   render() {
     return (
       <div>
-        <progress value={this.state.uploadValue} max="100" />
+        <progress value={this.state.uploadValue} max="100">
+          {this.state.uploadValue} %
+        </progress>
         <br />
-        <input type="file" onChange={this.props.onUpload} />
+        <input type="file" onChange={this.handleUpload} />
         <br />
         <img width="320" src={this.state.picture} alt="" />
       </div>
